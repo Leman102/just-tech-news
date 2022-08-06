380b528ffd01ce93e40497554426e7a83e2f77a6
Leoman <leo.pinzon102@gmail.com>
2022-08-05 21:40:57 -0400

    add homepage

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -35,7 +35,7 @@ router.delete('/:id', (req, res) => {
     })
         .then(dbCommentData => {
             if (!dbCommentData) {
-                res.status(404).json({ dbCommentData: 'No comment found with this id' });
+                res.status(404).json({ message: 'No comment found with this id' });
                 return;
             }
             res.json(dbCommentData);
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//add express static an take all of the contents of a folder and serve them as static assets
+//This is useful for front-end specific files like images, style sheets, and JavaScript files.
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+//set up handlebars.js as thr spp's template engine of choice
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 // sequelize.sync() method to establish the connection to the database
